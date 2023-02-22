d9ac7c5568e70566d3450bed59eb99ae822cb0ee
Dogs-Egregore <Junior3338wilson@gmail.com>
2023-02-21 19:43:36 -0700

    remove node modules and implement page navigation

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const puppeteer = require("puppeteer");
 
 
 (async () => {
-  const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser'
+  const browser = await puppeteer.launch({headless: false
   })
   const page = await browser.newPage();
-  await page.goto('https://mealprepmanual.com/recipes/');
-  // other actions...
+  await page.goto('https://mealprepmanual.com/');
 
-  await browser.close();
+  // other actions...
+  await page.mainFrame().click('xpath/html/body/div[1]/div/header/div[1]/div[3]/a/span[1]'
+  );
+  await page.waitForSelector('xpath/html/body/div[1]/div/header/div[2]/nav/ul/li[2]/a');
+  await page.mainFrame().click('xpath/html/body/div[1]/div/header/div[2]/nav/ul/li[2]/a');
+
 })();
